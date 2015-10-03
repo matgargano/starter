5f5e64942488ee393364ce132adb75cc35127fff
Mat Gargano <mgargano@gmail.com>
2015-10-02 22:00:59 -0400

    update

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-set :stage, :staging
-set :deploy_to, -> { "/home/ubuntu/#{fetch(:application)}/public" }
-set :dir, "/home/ubuntu/#{fetch(:application)}/public/current/web/"
-
-server '{{SERVER}}', user: 'ubuntu', roles: %w{web app db}
-
-
-fetch(:default_env).merge!(wp_env: :staging)
--- config/deploy/staging.rb
+++
@@ -1,7 +0,0 @@
-set :stage, :staging
-set :deploy_to, -> { "/var/www/#{fetch(:application)}" }
-set :dir, "/var/www/#{fetch(:application)}/current/web"
-server '{{SERVER}}', user: 'www-data', roles: %w{web app db}
-
-
-fetch(:default_env).merge!(wp_env: :staging)
